62764faf619198de70d6a255c9472c3b61f91751
Suchith <ksuchith131@gmail.com>
2026-08-26 18:52:28 +0530

    Task-01

---
+++ Task 1/List_and_Tuple.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3,4,5]
+print(numbers)
+numbers.append(6)
+print(numbers)
+
+numbers_2 = (1,2,3,4)
+print(numbers_2)
+# numbers_2.append(5)
+# print(numbers_2)
---
+++ Task 1/ReverseString.py
@@ -0,0 +1,3 @@
+text = input("Enter a string to be Reversed: ")
+reversed_string = text[::-1]
+print(reversed_string)
